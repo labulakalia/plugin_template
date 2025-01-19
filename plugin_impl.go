eca9b151045eb6df5df03e336b5f9d9f6768a22f
labulakalia <labulakalia@gmail.com>
2025-01-19 23:01:37 +0800

    upgrade plugin_api

--- plugin_impl.go
+++ plugin_impl.go
@@ -14,46 +14,46 @@ type PluginImpl struct {
 }
 
 // Id implements IPlugin.
-func (p *PluginImpl) PluginId() string {
+func (p *PluginImpl) PluginId() (string, error) {
 	panic("unimplemented")
 }
 
 // GetAuthType implements IPlugin.
-func (p *PluginImpl) GetAuthType() *plugin.AuthType {
+func (p *PluginImpl) GetAuthType() (*plugin.AuthType, error) {
 	panic("unimplemented")
 }
 
 // CheckAuth implements IPlugin.
-func (p *PluginImpl) CheckAuth(*plugin.AuthType) *plugin.Status {
+func (p *PluginImpl) CheckAuth(*plugin.AuthType) (*plugin.Status, error) {
 	panic("unimplemented")
 }
 
 // GetAuthData implements IPlugin.
-func (p *PluginImpl) GetAuthData() []byte {
+func (p *PluginImpl) GetAuthData() ([]byte, error) {
 	panic("unimplemented")
 }
 
 // InitAuth implements IPlugin.
-func (p *PluginImpl) InitAuth([]byte) *plugin.Status {
+func (p *PluginImpl) CheckAuthData([]byte) (*plugin.Status, error) {
 	panic("unimplemented")
 }
 
 // AuthId implements IPlugin.
-func (p *PluginImpl) PluginAuthId() string {
+func (p *PluginImpl) PluginAuthId() (string, error) {
 	panic("unimplemented")
 }
 
 // GetDirEntry implements IPlugin.
-func (p *PluginImpl) GetDirEntry(dir_path string, page uint64, page_size uint64) *plugin.DirEntry {
+func (p *PluginImpl) GetDirEntry(dir_path string, page uint64, page_size uint64) (*plugin.DirEntry, error) {
 	panic("unimplemented")
 }
 
 // GetFileResource implements IPlugin.
-func (p *PluginImpl) GetFileResource(file_path string) *plugin.FileResource {
+func (p *PluginImpl) GetFileResource(file_path string) (*plugin.FileResource, error) {
 	panic("unimplemented")
 }
 
 // Close implements IPlugin.
-func (p *PluginImpl) Close() {
+func (p *PluginImpl) Close() error {
 	panic("unimplemented")
 }
